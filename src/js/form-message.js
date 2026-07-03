40059f4cddc6f6b4051681da14dc48f42b609955
Darman <saintenough1992@gmail.com>
2026-07-03 22:33:08 +0300

    completed section w/out modal

--- src/js/form-message.js
+++ src/js/form-message.js
@@ -2,13 +2,18 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { createOrder } from './message-api';
-
 import { showLoader, hideLoader } from './loader';
 
+// Contact form element
 const form = document.querySelector('#contact-form');
+
+// Submit button
 const submitBtn = form.querySelector('.btn-submit');
+
+// Form loader overlay
 const formLoader = document.querySelector('.contact-form-loader');
 
+// Validate form fields
 function validateForm(data) {
   const errors = {};
 
@@ -19,12 +24,15 @@ function validateForm(data) {
   if (data.name.length < 2) {
     errors['name-input'] = 'Name has to be two or more characters long';
   }
+
   if (!/^[0-9]{12}$/.test(data.phone)) {
     errors['phone-input'] = 'Phone must contain 12 digits';
   }
 
   return errors;
 }
+
+// Get form values
 function getFormData(form) {
   return {
     name: form.elements['name-input'].value.trim(),
@@ -33,6 +41,7 @@ function getFormData(form) {
   };
 }
 
+// Display validation errors
 function showValidationErrors(errors) {
   clearValidationErrors();
 
@@ -45,6 +54,7 @@ function showValidationErrors(errors) {
   });
 }
 
+// Clear validation state
 function clearValidationErrors() {
   const fields = form.querySelectorAll('input, textarea');
   const errorTexts = form.querySelectorAll('.error-text');
@@ -53,14 +63,9 @@ function clearValidationErrors() {
   errorTexts.forEach(error => (error.textContent = ''));
 }
 
-form.addEventListener('submit', handleSubmit);
-
+// Handle form submission
 async function handleSubmit(event) {
   event.preventDefault();
-
-  const name = form.elements['name-input'].value.trim();
-  const phone = form.elements['phone-input'].value.trim();
-  const message = form.elements['message-input'].value.trim();
 
   const orderData = getFormData(form);
   const errors = validateForm(orderData);
@@ -73,6 +78,7 @@ async function handleSubmit(event) {
   clearValidationErrors();
 
   try {
+    // Show loading state
     showLoader(formLoader);
 
     await createOrder(orderData);
@@ -88,10 +94,14 @@ async function handleSubmit(event) {
       message:
         error.response?.data?.message ||
         error.message ||
-        `Something went wrong`,
+        'Something went wrong',
       position: 'topRight',
     });
   } finally {
+    // Hide loading state
     hideLoader(formLoader);
   }
 }
+
+// Listen for form submit
+form.addEventListener('submit', handleSubmit);
