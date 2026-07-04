ab75915098058f1e21e311683c52968fd43e4d7e
Andrii_Belmeha <iiitpix@hotmail.com>
2026-07-04 17:27:17 +0200

    added svg before title, added loader for requests, done desktop part

--- src/js/portfolio-api.js
+++ src/js/portfolio-api.js
@@ -1,38 +1,60 @@
 import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const categoryList = document.querySelector('.portfolio-category');
-const imagesList = document.querySelector('.portfolio-images');
-const buttonElement = document.querySelector('.show-more-button');
+const portfoliSectionElement = document.querySelector('.portfolio-section');
+
+const categoryList = portfoliSectionElement.querySelector(
+  '.portfolio-category'
+);
+const imagesList = portfoliSectionElement.querySelector('.portfolio-images');
+const buttonElement = portfoliSectionElement.querySelector('.show-more-button');
+const loaderElement = portfoliSectionElement.querySelector('.loader');
+const loaderWrapperElement = portfoliSectionElement.querySelector(
+  '.portfolio-loader-wrapper'
+);
 
 let selectedCategory = document.querySelector('#all-photos');
 
 const LIMIT_IMAGE_PER_PAGE = 9;
 const LIMIT_IMAGES_PER_SHOW_MORE = 3;
+
 let countTotalImages = 0;
 let countShowedImages = 0;
-
 let countShowMoreClicks = 0;
+
+renderCategories();
+renderBasicImages();
 
 categoryList.addEventListener('click', handleClickCategory);
 buttonElement.addEventListener('click', handleShowMoreClick);
 
 async function handleShowMoreClick() {
   countShowMoreClicks++;
-  const data = await getWeddingPhotos(
-    selectedCategory.id,
-    LIMIT_IMAGES_PER_SHOW_MORE + countShowMoreClicks,
-    LIMIT_IMAGES_PER_SHOW_MORE
-  );
-  const list = data.weddingPhotos.map(
-    img => `<li><img src="${img.img}" alt="${img.title}"/></li>`
-  );
-  imagesList.insertAdjacentHTML('beforeend', list.join(''));
+  try {
+    showLoader(loaderWrapperElement);
+    const data = await getWeddingPhotos(
+      selectedCategory.id,
+      LIMIT_IMAGES_PER_SHOW_MORE + countShowMoreClicks,
+      LIMIT_IMAGES_PER_SHOW_MORE
+    );
+    const list = data.weddingPhotos.map(
+      img => `<li><img src="${img.img}" alt="${img.title}"/></li>`
+    );
+    imagesList.insertAdjacentHTML('beforeend', list.join(''));
+  } catch {
+    iziToast.error({
+      message: 'Something went wrong with request',
+      position: 'bottomRight',
+    });
+  } finally {
+    hideLoader(loaderWrapperElement);
+  }
 }
 function handleClickCategory(e) {
   countShowMoreClicks = 0;
   countShowedImages = 0;
   countTotalImages = 0;
-  console.log(e.target.tagName);
 
   if (e.target.tagName !== 'LI') {
     return;
@@ -47,6 +69,51 @@ function handleClickCategory(e) {
     renderBasicImages(e.target.id);
   }
 }
+
+async function renderCategories() {
+  try {
+    const categories = await getCategories();
+    const list = categories.map(
+      img => `<li id=${img._id}>${img.category}</li>`
+    );
+    categoryList.insertAdjacentHTML('beforeend', list.join(''));
+  } catch {
+    iziToast.error({
+      message: 'Something went wrong with Category request',
+      position: 'bottomRight',
+    });
+  }
+}
+
+async function renderBasicImages(category = undefined) {
+  imagesList.innerHTML = '';
+  try {
+    showLoader(loaderWrapperElement);
+    const images = await getWeddingPhotos(category);
+    const list = images.weddingPhotos.map(
+      img => `<li><img src="${img.img}" alt="${img.title}"/></li>`
+    );
+    imagesList.insertAdjacentHTML('beforeend', list.join(''));
+  } catch {
+    iziToast.error({
+      message: 'Something went wrong with request',
+      position: 'bottomRight',
+    });
+  } finally {
+    hideLoader(loaderWrapperElement);
+  }
+}
+
+// LOADER
+
+export function showLoader(loader) {
+  loader.classList.remove('is-hidden');
+}
+export function hideLoader(loader) {
+  loader.classList.add('is-hidden');
+}
+
+// REQUESTS
 
 async function getCategories() {
   const response = await axios.get(
@@ -73,7 +140,6 @@ async function getWeddingPhotos(
       },
     }
   );
-  console.log(response.data);
   countTotalImages = response.data.totalItems;
   countShowedImages += response.data.weddingPhotos.length;
   if (countShowedImages === countTotalImages) {
@@ -82,20 +148,4 @@ async function getWeddingPhotos(
     buttonElement.disabled = false;
   }
   return response.data;
-}
-renderCategories();
-renderBasicImages();
-async function renderCategories() {
-  const categories = await getCategories();
-  const list = categories.map(img => `<li id=${img._id}>${img.category}</li>`);
-  categoryList.insertAdjacentHTML('beforeend', list.join(''));
-}
-
-async function renderBasicImages(category = undefined) {
-  imagesList.innerHTML = '';
-  const images = await getWeddingPhotos(category);
-  const list = images.weddingPhotos.map(
-    img => `<li><img src="${img.img}" alt="${img.title}"/></li>`
-  );
-  imagesList.insertAdjacentHTML('beforeend', list.join(''));
 }
